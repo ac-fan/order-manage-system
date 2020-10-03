289c62d88863ef752c1a4646ff71f0ed5c3cffdb
sve1r <plekiyeon@gmail.com>
2020-10-03 13:18:38 +0800

    默认更改列表

---
+++ test/test.java
@@ -0,0 +1,15 @@
+/**
+ * Class test
+ *
+ * @author sve1r
+ * @description
+ * @date 2020/10/3
+ */
+
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("This is a test console application");
+    }
+
+}
