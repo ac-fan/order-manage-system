3353679ef5a3b19ada828927ea2cc64a7e4f087a
sve1r <plekiyeon@gmail.com>
2020-10-04 19:47:31 +0800

    添加测试类

---
+++ src/com/qst/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.qst.dao;
+
+/**
+ * Class UserDao
+ *
+ * @author sve1r
+ * @description UserDao
+ * @date 2020/10/4
+ */
+
+
+public class UserDao {
+
+    public void selectUserById() {
+        //TODO
+    }
+}
---
+++ src/com/qst/service/UserService.java
@@ -0,0 +1,17 @@
+package com.qst.service;
+
+import com.qst.entity.User;
+
+/**
+ * Interface UserService
+ *
+ * @author sve1r
+ * @description 用户服务接口
+ * @date 2020/10/4
+ */
+
+
+public interface UserService {
+
+    User userLogin();
+}
