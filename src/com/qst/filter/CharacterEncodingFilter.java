9e786da32b25c65c3615c4629b240eb2cf71e943
sve1r <plekiyeon@gmail.com>
2020-10-06 11:03:16 +0800

    添加 字符编码过滤器

---
+++ src/com/qst/filter/CharacterEncodingFilter.java
@@ -0,0 +1,23 @@
+package com.qst.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class CharacterEncodingFilter implements Filter {
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
