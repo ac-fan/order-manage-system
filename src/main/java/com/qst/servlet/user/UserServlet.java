63097585123e615a498efc42048f9dfbff5edcc7
Administrator <1171239686@qq.com>
2020-10-09 16:26:34 +0800

    Merge branch 'test' of D:\IDEA\IntelliJ IDEA 2019.3.5\orderSystem with conflicts.

--- src/main/java/com/qst/servlet/user/UserServlet.java
+++ src/main/java/com/qst/servlet/user/UserServlet.java
@@ -1,7 +1,6 @@
 package com.qst.servlet.user;
 
 import com.alibaba.fastjson.JSONArray;
-
 import com.mysql.cj.util.StringUtils;
 import com.qst.pojo.User;
 import com.qst.service.user.UserService;
@@ -21,9 +20,9 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String method = req.getParameter("method");
-        if (method != null && method.equals("savepwd")) {
-            this.updatePwd(req, resp);
-        } else if (method != null && method.equals("pwdmodify")) {
+        if (method!=null&&method.equals("savepwd")){
+            this.updatePwd(req,resp);
+        }else if (method!=null&&method.equals("pwdmodify")){
             this.pwdModify(req, resp);
         }
     }
@@ -34,24 +33,24 @@ public class UserServlet extends HttpServlet {
     }
 
     //修改密码
-    public void updatePwd(HttpServletRequest req, HttpServletResponse resp) {
+    public void updatePwd(HttpServletRequest req, HttpServletResponse resp){
         //从Session里面拿ID;
         Object o = req.getSession().getAttribute(Constants.USER_SESSION);
 
         String newpassword = req.getParameter("newpassword");
 
-        System.out.println("UserServlet:" + newpassword);
+        System.out.println("UserServlet:"+newpassword);
 
         boolean flag = false;
 
-        System.out.println(o != null);
+        System.out.println(o!=null);
         System.out.println(StringUtils.isNullOrEmpty(newpassword));
 
-        if (o != null && newpassword != null) {
+        if (o!=null && newpassword!=null){
             UserService userService = new UserServiceImpl();
             flag = userService.updatePwd(((User) o).getId(), newpassword);
-            if (flag) {
-                req.setAttribute("message", "修改密码成功，请退出，使用新密码登录");
+            if (flag){
+                req.setAttribute("message","修改密码成功，请退出，使用新密码登录");
                 //密码修改成功，移除当前Session
                 req.getSession().removeAttribute(Constants.USER_SESSION);
                 try {
@@ -60,16 +59,16 @@ public class UserServlet extends HttpServlet {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            } else {
-                req.setAttribute("message", "密码修改失败");
+            }else {
+                req.setAttribute("message","密码修改失败");
                 //密码修改成功，移除当前Session
             }
-        } else {
-            req.setAttribute("message", "新密码有问题");
+        }else {
+            req.setAttribute("message","新密码有问题");
         }
 
         try {
-            req.getRequestDispatcher("pwdmodify.jsp").forward(req, resp);
+            req.getRequestDispatcher("pwdmodify.jsp").forward(req,resp);
         } catch (ServletException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -78,25 +77,25 @@ public class UserServlet extends HttpServlet {
     }
 
     //验证旧密码,session中有用户的密码
-    public void pwdModify(HttpServletRequest req, HttpServletResponse resp) {
+    public void pwdModify(HttpServletRequest req, HttpServletResponse resp){
         //从Session里面拿ID;
 
         Object o = req.getSession().getAttribute(Constants.USER_SESSION);
         String oldpassword = req.getParameter("oldpassword");
         System.out.println(oldpassword);
         //万能的Map : 结果集
-        Map<String, String> resultMap = new HashMap<String, String>();
+        Map<String, String> resultMap = new HashMap<String,String>();
 
-        if (o == null) { //Session失效了，session过期了
-            resultMap.put("result", "sessionerror");
-        } else if (StringUtils.isNullOrEmpty(oldpassword)) { //输入的密码为空
-            resultMap.put("result", "error");
-        } else {
+        if (o==null){ //Session失效了，session过期了
+            resultMap.put("result","sessionerror");
+        }else if (StringUtils.isNullOrEmpty(oldpassword)){ //输入的密码为空
+            resultMap.put("result","error");
+        }else {
             String userPassword = ((User) o).getUserPassword(); //Session中用户的密码
-            if (oldpassword.equals(userPassword)) {
-                resultMap.put("result", "true");
-            } else {
-                resultMap.put("result", "false");
+            if (oldpassword.equals(userPassword)){
+                resultMap.put("result","true");
+            }else {
+                resultMap.put("result","false");
             }
         }
 
