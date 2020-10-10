fe628a44c560f31acf2d36aab240db0c867a574b
sve1r <plekiyeon@gmail.com>
2020-10-10 11:43:40 +0800

    增加测试类

---
+++ test/test.java
@@ -0,0 +1,14 @@
+/**
+ * Class test
+ *
+ * @author sve1r
+ * @description
+ * @date 2020/10/5
+ */
+
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("This is Test Class");
+    }
+}
