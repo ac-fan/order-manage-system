364c9ab212c367edd5da938a25342e9bbbc96d4e
sve1r <plekiyeon@gmail.com>
2020-10-14 15:33:02 +0800

    修复 新增订单 的 js

--- web/static/js/billadd.js
+++ web/static/js/billadd.js
@@ -10,6 +10,9 @@ function priceReg(value) {
     return value;
 }
 
+function submitForm(){
+    $("#billForm").submit();
+}
 
 /*$(function () {
     billCode = $("#billCode");
@@ -138,22 +141,38 @@ var formControls = function () {
                 },
                 plugins: {
                     trigger: new FormValidation.plugins.Trigger(),
-                    // Bootstrap Framework Integration
+                    // Bootstrap 框架整合
                     bootstrap: new FormValidation.plugins.Bootstrap(),
-                    // Validate fields when clicking the Submit button
+                    // 点击提交按钮时进行表单校验
                     submitButton: new FormValidation.plugins.SubmitButton(),
-                    // Submit the form when all fields are valid
-                    defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
                 }
             }
-        );
+        ).on('core.form.valid',function(){
+            Swal.fire({
+                title: "确认提交",    //提示标题
+                text: "你确认要提交数据吗?",      //提示内容
+                icon: "warning",            //上端图标类型
+                showCancelButton: true,     //是否展示取消按钮
+                cancelButtonText: "不,我再想想.", //取消按钮文本
+                confirmButtonText: "确认提交" //确认按钮文本
+            }).then(function (result) {
+                if (result.value) {
+                    //提交表单
+                    submitForm();
+                    Swal.fire("已提交","订单提交成功","success")
+                } else if (result.dismiss === "cancel") {
+                    //选择取消按钮执行的操作
+                    Swal.fire("已取消该操作", "操作已取消:)", "error")
+                }
+            });
+        })
     }
 
     /**
      * 点击确认提交按钮时弹出提示
      */
-    addBtn.on("click", function () {
-
+    /*addBtn.on("click", function () {
+        validateForm();
         Swal.fire({
             title: "确认提交",    //提示标题
             text: "你确认要提交数据吗?",      //提示内容
@@ -164,13 +183,13 @@ var formControls = function () {
         }).then(function (result) {
             if (result.value) {
                 //选择确认执行的操作
-                $("#billForm").submit();
+                submitForm();
             } else if (result.dismiss === "cancel") {
                 //选择取消按钮执行的操作
                 Swal.fire("已取消该操作", "操作已取消:)", "error")
             }
         });
-    });
+    });*/
 
     return {
         init: function () {
